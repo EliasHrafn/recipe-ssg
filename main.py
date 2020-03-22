2b9791c09276ed1748ac01c4da23c35572fcb3ee
gjg <gjg@tskoli.is>
2020-03-22 11:25:27 +0000

    total reset

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
+import codecs
 from datetime import datetime
 from jinja2 import Environment, PackageLoader
 from markdown2 import markdown
-
 
 POSTS = {}
 
@@ -12,7 +12,6 @@ for markdown_post in os.listdir('content'):
     with open(file_path, 'r') as file:
         POSTS[markdown_post] = markdown(file.read(), extras=['metadata'])
 
-    
 POSTS = {
     post: POSTS[post] for post in sorted(POSTS, key=lambda post: datetime.strptime(POSTS[post].metadata['date'], '%Y-%m-%d'), reverse=True)
 }
@@ -22,14 +21,16 @@ index_template = env.get_template('index.html')
 bread_template = env.get_template('bread.html')
 post_template = env.get_template('post.html')
 
+# forsíðan er ekki með MD post renderingu
+index_html = index_template.render()
+
+# brauðuppskriftir
 posts_metadata = [POSTS[post].metadata for post in POSTS]
 tags = [post['tags'] for post in posts_metadata]
 bread_html = bread_template.render(posts=posts_metadata, tags=tags)
 
-index_html = index_template.render()
-
-with open ('../recipes/index.html','w',encoding='utf-8') as file:
-    file.write(index_html)   
+with open('../recipes/index.html', 'w', encoding='utf-8') as file:
+    file.write(index_html)
 
 with open('../recipes/bread.html', 'w') as file:
     file.write(bread_html)
@@ -41,7 +42,7 @@ for post in POSTS:
         'content': POSTS[post],
         'title': post_metadata['title'],
         'date': post_metadata['date'],
-        'thumbnail':post_metadata['thumbnail']
+        'thumbnail': post_metadata['thumbnail']
     }
 
     post_html = post_template.render(post=post_data)
@@ -50,3 +51,4 @@ for post in POSTS:
     os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
     with open(post_file_path, 'w') as file:
         file.write(post_html)
+
